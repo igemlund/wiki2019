5c003e0c2a055816f2d3603841b5741225305dd4
Arvid Larsson <arvla626@student.liu.se>
2019-04-18 10:03:04 +0200

    Toc now working.

--- src/bundle.js
+++ src/bundle.js
@@ -816,12 +816,14 @@ function jump (target, options) {
 var tocbot = require('tocbot');
 console.log("hej");
 tocbot.init({
-  // Where to render the table of contents.
-  tocSelector: '.js-toc',
-  // Where to grab the headings to build the table of contents.
-  contentSelector: '.js-toc-content',
-  // Which headings to grab inside of the contentSelector element.
-  headingSelector: 'h1, h2, h3',
+tocSelector: '.js-toc',
+// Where to grab the headings to build the table of contents.
+contentSelector: '#text',
+// Which headings to grab inside of the contentSelector element.
+headingSelector: 'h1, h2, h3',
 });
+
+
+
 
 },{"tocbot":3}]},{},[6]);
