2226c7f155933e36537c78f8f5bdae35401dba50
Arvid Larsson <arvla626@student.liu.se>
2019-04-19 17:50:17 +0200

    Refraction.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 const gulp         = require('gulp');
-const browserSync  = require('browser-sync').create();
+const browserSync  = require('browser-sync');
 const sass         = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
-const tocbot       = require('tocbot');
 const jquery       = require('jquery');
 
+const config = {
+   teamName: 'Lund',
+};
 
-// Compile Sass & Inject Into Browser
+
+// Build Tasks
 gulp.task('sass', function() {
     return gulp.src(['app/scss/*.scss'])
         .pipe(sass())
@@ -18,17 +21,68 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
-
-// Watch Sass & Serve
-gulp.task('serve', ['sass'], function() {
-    browserSync.init({
-        server: "./build"  
-    });
-
-    gulp.watch(['app/scss/*.scss'], ['sass']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
-    gulp.watch("app/*.js").on('change', browserSync.reload);
+gulp.task('pages', function() {
+    return gulp.src(['app/pages/*.html'])
+        .pipe(gulp.dest("build/"))
+        .pipe(browserSync.stream());
 });
 
+gulp.task('assets', () => {
+  return gulp.src(['app/assets/*'])
+    .pipe(gulp.dest('build/'))
+});
+
+gulp.task('scripts', () => {
+  return gulp.src('app/bundle.js')
+    .pipe(gulp.dest('build/'));
+});
+
+// Watch Tasks
+gulp.task('sync-pages', ['pages'], (done) => {
+  browserSync.reload();
+  done();
+});
+
+gulp.task('sync-scripts', ['scripts'], (done) => {
+  browserSync.reload();
+  done();
+});
+
+gulp.task('sync-assets', ['assets'], (done) => {
+  browserSync.reload();
+  done();
+});
+
+gulp.task('browser-sync', ['build'], () => {
+  browserSync.init({
+    //startPath: `/Team:${config.teamName}`,
+    server: {
+      baseDir: 'build/',
+      serveStaticOptions: {
+        extensions: ['html', 'css', 'min.css', 'js', 'min.js']
+      },
+     // middleware: (req,res,next) => {
+     //   // Reroute wiki url to directory in dist
+     //   req.url = req.url.replace(/^\/Team:[^/]+/, '/pages');
+     //   req.url = req.url.replace(/^\/File:/, `/assets/`);
+     //   return next();
+     // },
+    },
+    ghostMode: false,
+  });
+});
+
+gulp.task('watch', ['build', 'browser-sync'], () => {
+   gulp.watch('app/pages/*.html', ['sync-pages']);
+   gulp.watch('app/*.js', ['sync-scripts']);
+   gulp.watch('app/assets/*', ['sync-assets']);
+   gulp.watch('app/scss/*.scss', ['sass']);
+ });
+
+// Watch & Serve
+gulp.task('serve', ['pages', 'assets', 'scripts','sass']);
+
 // Default Task
-gulp.task('default', ['serve']);
+gulp.task('build', ['pages', 'assets', 'scripts', 'sass']);
+
+gulp.task('default', ['serve', 'watch']);
