a05a4e454a3f194b825bad58fb334fc4e7952948
Arvid Larsson <arvla626@student.liu.se>
2019-07-09 14:32:13 +0200

    New navbar

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -2,6 +2,7 @@ var tocbot = require('tocbot');
 var $ = require('jquery');
 var katex = require('katex');
 
+// Table of Contents
 tocbot.init({
     tocSelector: '.js-toc',
     contentSelector: '#body',
@@ -10,33 +11,29 @@ tocbot.init({
 });
 
 $(window).scroll(function(){
-
     var stickyOffset = $('.logo-container').offset().top + $('.logo-container').height() - $('.navbar-background').height();
     var sticky = $('.sticky'),
         scroll = $(window).scrollTop();
-
     if (scroll >= stickyOffset) sticky.addClass('fixed');
     else sticky.removeClass('fixed');
 });
 
-  // Get all <div or span or p class ="maths"> elements in the document
-	var x = document.getElementsByClassName('maths');
 
-	  // go through each of them in turn
-	for (var i = 0; i < x.length; i++) {
-console.log("test");
-	try {
-		if(x[i].tagName == "DIV"){
-			t= katex.render(x[i].textContent,x[i],{ displayMode: true });
-		} else {
-            
-			t= katex.render(x[i].textContent,x[i]);
-		}
-	}
-	catch(err) {
-		x[i].style.color = 'red';
-		x[i].textContent= err;
-		}
+// Maths
+var x = document.getElementsByClassName('maths');
+for (var i = 0; i < x.length; i++) {
+    console.log("test");
+    try {
+        if(x[i].tagName == "DIV"){
+            t= katex.render(x[i].textContent,x[i],{ displayMode: true });
+        } else {
 
-	}
+            t= katex.render(x[i].textContent,x[i]);
+        }
+    }
+    catch(err) {
+        x[i].style.color = 'red';
+        x[i].textContent= err;
+    }
+}
 
